1aff213a112a4acd24eae163fa5b701bcea21a0b
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-03-14 00:15:02 -0700

    landing page component abstraction done

---
+++ src/components/CategoryNavTabs/CategoryNavTabs.jsx
@@ -0,0 +1,28 @@
+function CategoryNavTab() {
+  return (
+    <section class="categories_navTab_container">
+      <ul class="categories_navTabs_list">
+        <li>
+          <a
+            class="categories_navLink categories_navLink1"
+            href="/new-arrivals"
+          >
+            New Arrivals
+          </a>
+        </li>
+        <li>
+          <a class="categories_navLink categories_navLink2" href="/bestsellers">
+            Best Sellers
+          </a>
+        </li>
+        <li>
+          <a class="categories_navLink categories_navLink2" href="/trending">
+            Trending
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default CategoryNavTab;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer.jsx";
 import VerticalCard from "./Card/VerticalCard/VerticalCard";
 import HeroBanner from "./Banner/HeroBanner/HeroBanner.jsx";
 import PromotionBanner from "./Banner/PromotionBanner/PromotionBanner.jsx";
+import CategoryNavTab from "./CategoryNavTabs/CategoryNavTabs.jsx";
 
-export {Navbar, Footer, VerticalCard, HeroBanner, PromotionBanner};
+export {Navbar, Footer, VerticalCard, HeroBanner, PromotionBanner, CategoryNavTab};
